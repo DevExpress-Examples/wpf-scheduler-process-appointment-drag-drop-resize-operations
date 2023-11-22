b6ca50be2616b6a8529fd327286a5e8ad24e36f0
DevExpressExampleBot <fakeEmail>
2023-11-22 18:06:23 +0400

    Source auto update [skip ci]

--- VB/SchedulerDragDropResizeExample/Models/Doctor.vb
+++ VB/SchedulerDragDropResizeExample/Models/Doctor.vb
@@ -1,26 +1,27 @@
-﻿#Region "#usings"
+#Region "#usings"
 Imports DevExpress.Mvvm.POCO
-Imports System
-#End Region ' #usings
 
+#End Region  ' #usings
 Namespace SchedulerDragDropResizeExample
-	Public Class Doctor
-		Public Shared Function Create() As Doctor
-			Return ViewModelSource.Create(Function() New Doctor())
-		End Function
-		Public Shared Function Create(ByVal id As Integer, ByVal name As String) As Doctor
-'INSTANT VB NOTE: The variable doctor was renamed since it may cause conflicts with calls to static members of the user-defined type with this name:
-			Dim doctor_Conflict As Doctor = SchedulerDragDropResizeExample.Doctor.Create()
-			doctor_Conflict.Id = id
-			doctor_Conflict.Name = name
-			Return doctor_Conflict
-		End Function
 
-		Protected Sub New()
-		End Sub
+    Public Class Doctor
 
-		Public Overridable Property Id() As Integer
-		Public Overridable Property Name() As String
-	End Class
+        Public Shared Function Create() As Doctor
+            Return ViewModelSource.Create(Function() New Doctor())
+        End Function
+
+        Public Shared Function Create(ByVal id As Integer, ByVal name As String) As Doctor
+            Dim doctor As Doctor = Create()
+            doctor.Id = id
+            doctor.Name = name
+            Return doctor
+        End Function
+
+        Protected Sub New()
+        End Sub
+
+        Public Overridable Property Id As Integer
+
+        Public Overridable Property Name As String
+    End Class
 End Namespace
-
--- VB/SchedulerDragDropResizeExample/Models/PaymentState.vb
+++ VB/SchedulerDragDropResizeExample/Models/PaymentState.vb
@@ -1,23 +1,28 @@
-﻿Imports DevExpress.Mvvm.POCO
+Imports DevExpress.Mvvm.POCO
 
 Namespace SchedulerDragDropResizeExample
-	Public Class PaymentState
-		Public Shared Function Create() As PaymentState
-			Return ViewModelSource.Create(Function() New PaymentState())
-		End Function
-		Public Shared Function Create(ByVal id As Integer, ByVal caption As String, ByVal color As String) As PaymentState
-			Dim psi As PaymentState = PaymentState.Create()
-			psi.Id = id
-			psi.Caption = caption
-			psi.Color = color
-			Return psi
-		End Function
 
-		Protected Sub New()
-		End Sub
+    Public Class PaymentState
 
-		Public Overridable Property Id() As Integer
-		Public Overridable Property Caption() As String
-		Public Overridable Property Color() As String
-	End Class
+        Public Shared Function Create() As PaymentState
+            Return ViewModelSource.Create(Function() New PaymentState())
+        End Function
+
+        Public Shared Function Create(ByVal id As Integer, ByVal caption As String, ByVal color As String) As PaymentState
+            Dim psi As PaymentState = Create()
+            psi.Id = id
+            psi.Caption = caption
+            psi.Color = color
+            Return psi
+        End Function
+
+        Protected Sub New()
+        End Sub
+
+        Public Overridable Property Id As Integer
+
+        Public Overridable Property Caption As String
+
+        Public Overridable Property Color As String
+    End Class
 End Namespace
